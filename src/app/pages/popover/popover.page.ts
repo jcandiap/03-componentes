494543f7dd1fbb9efc306d48ff5c035a0a8a9cde
Jose Candia <josecandiap@gmail.com>
2020-09-21 22:11:03 -0300

    Se agrego el popover
    revisar porque no muestra los datos

---
+++ src/app/pages/popover/popover.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopoverInfoComponent } from '../../components/popover-info/popover-info.component';
+
+@Component({
+  selector: 'app-popover',
+  templateUrl: './popover.page.html',
+  styleUrls: ['./popover.page.scss'],
+})
+export class PopoverPage implements OnInit {
+
+  constructor(public popoverController: PopoverController) { }
+
+  ngOnInit() {
+  }
+
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: PopoverInfoComponent,
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
+
+}
